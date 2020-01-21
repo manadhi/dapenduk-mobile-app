8b194a0c82bd335e7fbced24bd03f40654fb1e97
udhi <udhi.permana@solusi247.com>
2020-01-21 17:40:14 +0700

    add greendao and model person

---
+++ app/src/main/java/com/udhipe/dapenduk/model/Person.java
@@ -0,0 +1,124 @@
+package com.udhipe.dapenduk.model;
+
+import androidx.annotation.InspectableProperty;
+import androidx.annotation.Keep;
+import androidx.annotation.Nullable;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Index;
+import org.greenrobot.greendao.annotation.NotNull;
+
+@Entity(indexes = {
+        @Index(value = "name, id DESC", unique = true)
+})
+
+public class Person {
+    @Id(autoincrement = true)
+    private Long id;
+
+    @NotNull
+    private String name;
+    @NotNull
+    private String address;
+    @NotNull
+    private String birthPlace;
+    @NotNull
+    private String birthDate;
+    @NotNull
+    private String gender;
+
+    @Nullable
+    private String photoPath;
+    @Nullable
+    private String profession;
+
+    @Generated(hash = 1024547259)
+    public Person() {
+    }
+
+    @Generated(hash = 181443522)
+    public Person(Long id, @NotNull String name, @NotNull String address, @NotNull String birthPlace,
+            @NotNull String birthDate, @NotNull String gender, String photoPath, String profession) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.birthPlace = birthPlace;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.photoPath = photoPath;
+        this.profession = profession;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    @NotNull
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(@NotNull String address) {
+        this.address = address;
+    }
+
+    @NotNull
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(@NotNull String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    @NotNull
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(@NotNull String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @NotNull
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(@NotNull String gender) {
+        this.gender = gender;
+    }
+
+    @Nullable
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(@Nullable String photoPath) {
+        this.photoPath = photoPath;
+    }
+
+    @Nullable
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(@Nullable String profession) {
+        this.profession = profession;
+    }
+}
