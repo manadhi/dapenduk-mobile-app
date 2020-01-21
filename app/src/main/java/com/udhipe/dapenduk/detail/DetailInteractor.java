7173757bbcd9f103b3a1545d2966fc568051deeb
udhi <udhi.permana@solusi247.com>
2020-01-21 20:29:41 +0700

    detail interactor

--- app/src/main/java/com/udhipe/dapenduk/detail/DetailInteractor.java
+++ app/src/main/java/com/udhipe/dapenduk/detail/DetailInteractor.java
@@ -5,6 +5,6 @@ import com.udhipe.dapenduk.model.Person;
 public class DetailInteractor implements DetailContract.Interactor {
     @Override
     public void loadOneData(Long id, Listener<Person> listener) {
-        
+
     }
 }
