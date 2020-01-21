59d40dd466944acbb58c9046db7d670d8b9b486b
udhi <udhi.permana@solusi247.com>
2020-01-22 02:29:14 +0700

    main layout

--- app/src/main/java/com/udhipe/dapenduk/main/MainInteractor.java
+++ app/src/main/java/com/udhipe/dapenduk/main/MainInteractor.java
@@ -1,13 +1,17 @@
 package com.udhipe.dapenduk.main;
 
+import com.udhipe.dapenduk.model.Person;
+
+import java.util.ArrayList;
+
 public class MainInteractor implements MainContract.Interactor {
     @Override
-    public void loadData(String filter) {
+    public void loadData(String filter, Listener<ArrayList<Person>> listener) {
 
     }
 
     @Override
-    public void deleteData(Long id) {
+    public void deleteData(Long id, Listener<Boolean> listener) {
 
     }
 }
